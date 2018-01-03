4c0c9c6ba366f6f961ebb84e8e973cc6b313470a
Axel Uhl <axel.uhl@sap.com>
2018-01-03 23:17:16 +0100

    persistence for chunks to tick

---
+++ src/uhl/mc/tickchunks/ChunksToKeepTicking.java
@@ -0,0 +1,85 @@
+package uhl.mc.tickchunks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import net.minecraft.server.ChunkCoordIntPair;
+
+/**
+ * Manages the collection of chunk coordinates that are to be kept ticking.
+ * Such an object can be constructed from a {@link YamlConfiguration} object
+ * if such object was produced by an instance of this class.
+ */
+public class ChunksToKeepTicking {
+	private final Map<Integer, Set<ChunkCoordIntPair>> coordinatesOfChunksToKeepTickingPerWorldDimension;
+	private static final String DATA_KEY = "world-dimensions";
+	private static final String X_KEY = "x";
+	private static final String Z_KEY = "z";
+	
+	public ChunksToKeepTicking() {
+		coordinatesOfChunksToKeepTickingPerWorldDimension = Collections.synchronizedMap(new HashMap<>());
+	}
+	
+	void addChunk(int worldDimension, int x, int z) {
+		Set<ChunkCoordIntPair> set = coordinatesOfChunksToKeepTickingPerWorldDimension.get(worldDimension);
+		if (set == null) {
+			set = new HashSet<>();
+			coordinatesOfChunksToKeepTickingPerWorldDimension.put(worldDimension, set);
+		}
+		set.add(new ChunkCoordIntPair(x, z));
+	}
+
+	void removeChunk(int worldDimension, int x, int z) {
+		Set<ChunkCoordIntPair> set = coordinatesOfChunksToKeepTickingPerWorldDimension.get(worldDimension);
+		if (set != null) {
+			set.remove(new ChunkCoordIntPair(x, z));
+			if (set.isEmpty()) {
+				coordinatesOfChunksToKeepTickingPerWorldDimension.remove(worldDimension);
+			}
+		}
+	}
+	
+	void load(YamlConfiguration data) {
+		final ConfigurationSection coordinates = data.getConfigurationSection(DATA_KEY);
+		for (final String dimensionNumberAsString : coordinates.getKeys(/* deep */ false)) {
+			final Set<ChunkCoordIntPair> chunkCoordinatePairs = new HashSet<>();
+			for (final Map<?, ?> coordinate : coordinates.getMapList(dimensionNumberAsString)) {
+				final ChunkCoordIntPair c = new ChunkCoordIntPair(((Number) coordinate.get(X_KEY)).intValue(), ((Number) coordinate.get(Z_KEY)).intValue());
+				chunkCoordinatePairs.add(c);
+			}
+			coordinatesOfChunksToKeepTickingPerWorldDimension.put(Integer.valueOf(dimensionNumberAsString), chunkCoordinatePairs);
+		}
+	}
+	
+	YamlConfiguration getData() {
+		final YamlConfiguration result = new YamlConfiguration();
+		final Map<String, List<Map<String, Integer>>> coordinatesAsMap = new HashMap<>();
+		synchronized (coordinatesOfChunksToKeepTickingPerWorldDimension) {
+			for (final Entry<Integer, Set<ChunkCoordIntPair>> c : coordinatesOfChunksToKeepTickingPerWorldDimension.entrySet()) {
+				final List<Map<String, Integer>> coordinatesList = new ArrayList<>();
+				for (final ChunkCoordIntPair coord : c.getValue()) {
+					final Map<String, Integer> mapForCoordinate = new HashMap<>();
+					mapForCoordinate.put(X_KEY, coord.x);
+					mapForCoordinate.put(Z_KEY, coord.z);
+					coordinatesList.add(mapForCoordinate);
+				}
+				coordinatesAsMap.put(c.getKey().toString(), coordinatesList);
+			}
+		}
+		result.createSection(DATA_KEY, coordinatesAsMap);
+		return result;
+	}
+
+	public Iterable<Map.Entry<Integer, Set<ChunkCoordIntPair>>> getTickingChunksPerWorldDimension() {
+		return Collections.unmodifiableMap(coordinatesOfChunksToKeepTickingPerWorldDimension).entrySet();
+	}
+}
--- src/uhl/mc/tickchunks/TickChunksPlugin.java
+++ src/uhl/mc/tickchunks/TickChunksPlugin.java
@@ -1,81 +1,159 @@
 package uhl.mc.tickchunks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.minecraft.server.ChunkCoordIntPair;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerChunk;
 import net.minecraft.server.PlayerChunkMap;
 import net.minecraft.server.World;
 import net.minecraft.server.WorldServer;
 
 public class TickChunksPlugin extends JavaPlugin {
+	private ChunksToKeepTicking chunksToKeepTicking;
+	private boolean debug;
+
+	public TickChunksPlugin() {
+		chunksToKeepTicking = new ChunksToKeepTicking();
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 0) {
-        	sender.sendMessage("Usage: /tc [ k[eep] | r[elease] ]");
-        } else {
+		final boolean result;
+        if (args.length == 1) {
 	        if (sender instanceof CraftPlayer) {
 	        	final CraftPlayer player = (CraftPlayer) sender;
 	        	final World world = player.getHandle().world;
 	        	if (world instanceof WorldServer) { // what else...?
-	        		final WorldServer worldServer = (WorldServer) world;
-	        		final PlayerChunkMap playerChunkMap = worldServer.getPlayerChunkMap();
 	        		final Chunk chunk = player.getLocation().getChunk();
-					final PlayerChunk playerChunk = playerChunkMap.getChunk(chunk.getX(), chunk.getZ());
+					int x = chunk.getX();
+					int z = chunk.getZ();
+					final WorldServer worldServer = (WorldServer) world;
+					final PlayerChunkMap playerChunkMap = worldServer.getPlayerChunkMap();
+					final PlayerChunk playerChunk = playerChunkMap.getChunk(x, z);
 					if (args[0].toLowerCase().startsWith("k")) {
-						if (playerChunk.isKeptLoadedEvenIfEmpty()) {
-							sender.sendMessage("Chunk ("+chunk.getX()+","+chunk.getZ()+") is already being kept in PlayerChunkMap.");
+						result = true;
+						if (playerChunk.isKeptTickingEvenIfEmpty()) {
+							sender.sendMessage("Chunk ("+x+","+z+") is already being kept in PlayerChunkMap.");
 						} else {
-							playerChunk.keepLoadedEvenIfEmpty();
-							sender.sendMessage("Chunk ("+chunk.getX()+","+chunk.getZ()+") is now being kept in PlayerChunkMap even when no player has it in sight.");
+							keepChunkTicking(((WorldServer) world).dimension, playerChunk);
+							playerChunk.keepTickingEvenIfEmpty();
+							sender.sendMessage("Chunk ("+x+","+z+") is now being kept in PlayerChunkMap even when no player has it in sight.");
+							final Plugin keepChunksPlugin = Bukkit.getServer().getPluginManager().getPlugin("KeepChunks");
+							if (keepChunksPlugin != null) {
+								if (debug) {
+									getLogger().info("Found KeepChunks plugin. Requesting chunk ["+playerChunk.chunk.locX+
+											","+playerChunk.chunk.locZ+"] to keep loaded");
+								}
+								String commandLine = "kc kc "+x+" "+z+" "+((WorldServer) world).getWorldData().getName();
+								final boolean keepChunksResult = Bukkit.getServer().dispatchCommand(sender,
+										commandLine);
+								if (debug) {
+									getLogger().info("KeepChunks command \""+commandLine+"\" returned "+keepChunksResult);
+								}
+							}
 						}
 					} else if (args[0].toLowerCase().startsWith("r")) {
-						if (!playerChunk.isKeptLoadedEvenIfEmpty()) {
-							sender.sendMessage("Chunk ("+chunk.getX()+","+chunk.getZ()+") was not being kept in PlayerChunkMap.");
+						result = true;
+						if (!playerChunk.isKeptTickingEvenIfEmpty()) {
+							sender.sendMessage("Chunk ("+x+","+z+") was not being kept in PlayerChunkMap.");
 						} else {
-							playerChunk.releaseIfEmpty();
-							sender.sendMessage("Chunk ("+chunk.getX()+","+chunk.getZ()+") is now being released from PlayerChunkMap when no player has it in sight.");
+							releaseChunk(((WorldServer) world).dimension, playerChunk);
+							sender.sendMessage("Chunk ("+x+","+z+") is now being released from PlayerChunkMap when no player has it in sight.");
+							if (Bukkit.getServer().getPluginManager().getPlugin("KeepChunks") != null) {
+								sender.sendMessage("Consider releasing the chunk also from the KeepChunks plugin using command /kc rc "+x+" "+z+
+										" "+((WorldServer) world).getWorldData().getName());
+							}
 						}
 					} else {
 						sender.sendMessage("Sub-Command "+args[0]+" not understood.");
+						result = false;
 					}
+	        	} else {
+	        		result = false;
 	        	}
+	        } else {
+	        	result = false;
 	        }
+        } else {
+        	result = false;
         }
-        return true;
+        return result;
 	}
 
-	@Override
-	public void onDisable() {
-		// TODO Auto-generated method stub
-		super.onDisable();
+	private void keepChunkTicking(int worldDimension, PlayerChunk playerChunk) {
+		playerChunk.keepTickingEvenIfEmpty();
+		chunksToKeepTicking.addChunk(worldDimension, playerChunk.chunk.locX, playerChunk.chunk.locZ);
+		updateDataFile();
+	}
+
+	private void releaseChunk(int worldDimension, PlayerChunk playerChunk) {
+		playerChunk.releaseIfEmpty();
+		chunksToKeepTicking.removeChunk(worldDimension, playerChunk.chunk.locX, playerChunk.chunk.locZ);
+		updateDataFile();
+	}
+
+	private void updateDataFile() {
+		try {
+			final FileWriter dataFileWriter = new FileWriter(getDataFile());
+			dataFileWriter.write(chunksToKeepTicking.getData().saveToString());
+			dataFileWriter.close();
+		} catch (IOException e) {
+			getLogger().severe("Couldn't write to data file "+getDataFile());
+		}
 	}
 
 	@Override
 	public void onEnable() {
-		// TODO Auto-generated method stub
-		super.onEnable();
+		if (debug) {
+			getLogger().info("onEnable");
+		}
+		for (final Entry<Integer, Set<ChunkCoordIntPair>> chunkCoordinatesForWorldDimension : chunksToKeepTicking.getTickingChunksPerWorldDimension()) {
+			@SuppressWarnings("deprecation") // how else can we obtain the server without using a deprecated method?
+			final WorldServer worldServer = MinecraftServer.getServer().getWorldServer(chunkCoordinatesForWorldDimension.getKey());
+			for (final ChunkCoordIntPair coord : chunkCoordinatesForWorldDimension.getValue()) {
+				if (debug) {
+					getLogger().info("keeping chunk "+coord+" ticking");
+				}
+				worldServer.getPlayerChunkMap().keepPlayerChunkTicking(coord.x, coord.z);
+			}
+		}
 	}
 
 	@Override
 	public void onLoad() {
-		// TODO Auto-generated method stub
-		super.onLoad();
+		saveDefaultConfig();
+		debug = getConfig().getBoolean("general.debug");
+		if (debug) {
+			getLogger().info("loading data...");
+		}
+		final File dataFile = getDataFile();
+		try {
+			if (dataFile.exists()) {
+				final YamlConfiguration data = YamlConfiguration.loadConfiguration(new FileReader(dataFile));
+				chunksToKeepTicking.load(data);
+			}
+		} catch (FileNotFoundException e) {
+			getLogger().warning("The data file "+dataFile+" just existed but still cannot be found. Not loading any data.");
+		}
 	}
 
-	@Override
-	public void saveConfig() {
-		// TODO Auto-generated method stub
-		super.saveConfig();
+	private File getDataFile() {
+		return new File(getDataFolder(), "data.yml");
 	}
-
-	@Override
-	public void saveDefaultConfig() {
-		// TODO Auto-generated method stub
-		super.saveDefaultConfig();
-	}
-
 }
